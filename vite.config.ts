836796678438be4bca67bfff52278e82b251ae88
Siddhesh Kadlag <captainlevi252@gmail.com>
2026-02-22 18:12:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Portfolio_V2/', // This tells the browser to look in your repo folder
+})
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
